205ec0f43f016112702c6bfb8ae328482dcf7e53
salihasariyildiz <saliha@saliha-MacBook-Air.local>
2022-09-05 13:45:19 +0300

    daireninAlaniVeCevresi eklendi

--- daireninAlaniVeCevresi.java
+++ daireninAlaniVeCevresi.java
@@ -1,2 +1,27 @@
-package Java101;public class Main {
-}
+package Java101;
+
+import java.util.Scanner;
+
+public class daireninAlaniVeCevresi {
+    public static void main(String[] args) {
+
+            int r;
+            double pi, alan, cevre,a,daireDilimininAlani;
+
+            Scanner inp = new Scanner(System.in);
+            System.out.print("Dairenin Yarıçapını Giriniz: ");
+            r= inp.nextInt();
+            System.out.print("Dairenin Açısını Giriniz: ");
+            a= inp.nextDouble();
+
+            pi= 3.14;
+            alan= pi*r*r;
+            cevre= 2*pi*r;
+            daireDilimininAlani= (pi*(r*r)*a)/360;
+
+            System.out.println("Dairenin Alanı: "+alan);
+            System.out.println("Dairenin Çevresi: "+cevre);
+            System.out.println("Daire Diliminin Alani: "+daireDilimininAlani);
+        }
+    }
+
