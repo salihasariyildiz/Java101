08052640d4af2b7241986c72134f5480c60b721a
salihasariyildiz <saliha@saliha-MacBook-Air.local>
2022-09-05 13:27:12 +0300

    taksimetreHesaplama eklendi

--- taksimetreHesaplama.java
+++ taksimetreHesaplama.java
@@ -1,2 +1,21 @@
-package Java101;public class taksimetreHesaplama {
+package Java101;
+import java.util.Scanner;
+
+public class taksimetreHesaplama {
+    public static void main(String[] args) {
+        int km;
+        double acilisUcreti, odenecekTutar;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Mesafeyi Km Cinsinden Giriniz:  ");
+        km=inp.nextInt();
+
+        acilisUcreti= 10;
+        odenecekTutar= acilisUcreti + km * 2.20;
+        odenecekTutar= (odenecekTutar<=20) ? 20 :(odenecekTutar);
+        System.out.println("Ödenecek Tutar:  "+odenecekTutar);
+
+
+
+    }
 }
