f54c73d841a909f181c4b56e8e766b81e8fd05fb
salihasariyildiz <sariyildizsaliha35@gmail.com>
2022-09-09 16:02:59 +0300

    Add files via upload

---
+++ SayilariKucuktenBuyugeSiralama.java
@@ -0,0 +1,38 @@
+package Java101;
+import java.util.Scanner;
+public class SayilariKucuktenBuyugeSiralama {
+    public static void main(String[] args) {
+        int a, b ,c;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("1.Sayıyı Giriniz: ");
+        a= inp.nextInt();
+        System.out.print("2.Sayıyı Giriniz: ");
+        b=inp.nextInt();
+        System.out.print("3.Sayıyı Giriniz: ");
+        c=inp.nextInt();
+
+        if ((a<b) && (a<c)){
+            if (b<c){
+                System.out.println("a<b<c");
+            }else{
+                System.out.println("a<c<b");
+            }
+        } else if ((b<a) && (b<c)) {
+            if (a<c){
+                System.out.println("b<a<c");
+            }else{
+                System.out.println("b<c<a");
+            }
+
+
+        }else{
+            if (a<b){
+                System.out.println("c<a<b");
+            }else {
+                System.out.println("c<b<a");
+            }
+        }
+    }
+}
